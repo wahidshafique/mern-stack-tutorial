838e7f47b0b171e66817f512905f319c2ea09474
Wahid Shafique <wahidshafique42@gmail.com>
2021-01-10 12:39:11 -0500

    use not-deprecetated tslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,11 @@ import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json({ limit: "30mb " }));
+app.use(bodyParser.urlencoded({ limit: "30mb ", extended: true }));
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("The sedulous hyena ate the antelope!");
 });
